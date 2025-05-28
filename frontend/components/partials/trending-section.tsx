739040c2b918452371f3330d4d049b06f5d5378e
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-05-28 22:18:34 +0300

    feat : trend section size

--- frontend/components/partials/trending-section.tsx
+++ frontend/components/partials/trending-section.tsx
@@ -3,7 +3,8 @@ import {} from '@radix-ui/react-slot'
 
 export function TrendingSection(){
     return(
-        <section className="min-h-96 bg-secondary">
+        <section className="min-h-96 bg-foreground w-[85%] p-5 m-auto flex flex-col ">
+            <p className="text-3xl text-gray-200 capitalize font-bold ">trending now</p>
             
         </section>
     )
