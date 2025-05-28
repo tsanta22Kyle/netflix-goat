d9579c7bfa59f54e31490c424343b98f9385cfc4
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-05-28 15:39:56 +0300

    trend section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/global/header";
 import { HeroSection } from "@/components/partials/hero-section";
+import { TrendingSection } from "@/components/partials/trending-section";
 
 export default function Home() {
   return (
     <>
       <Header />
       <HeroSection />
+      <TrendingSection/>
     </>
   );
 }
---
+++ frontend/components/partials/trending-section.tsx
@@ -0,0 +1,10 @@
+
+import {} from '@radix-ui/react-slot'
+
+export function TrendingSection(){
+    return(
+        <section className="min-h-96 bg-secondary">
+            
+        </section>
+    )
+}
